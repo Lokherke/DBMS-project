4ccfa93149cff524aee9b1239ea7f9bdc2a4b027
Neaya Chalise <117581603+Lokherke@users.noreply.github.com>
2026-01-29 01:33:34 +0545

    Update get_db_connection to use env variables
    Refactor database connection to use environment variables.

--- Backend/db.py
+++ Backend/db.py
@@ -1,9 +1,21 @@
 import mysql.connector
 
+'''
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
         password="14501245",
         database="stock_system"
+    )
+'''
+import os
+import mysql.connector
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
