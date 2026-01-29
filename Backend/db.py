e0ea5357b456fc2395472c93352f94ad73745f92
Neaya Chalise <117581603+Lokherke@users.noreply.github.com>
2026-01-29 11:04:46 +0545

    Clean up db.py by removing docstring
    Removed unnecessary docstring and database connection details.

--- Backend/db.py
+++ Backend/db.py
@@ -1,14 +1,3 @@
-'''
-import mysql.connector
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="14501245",
-        database="stock_system"
-    )
-'''
 import os
 import mysql.connector
 
